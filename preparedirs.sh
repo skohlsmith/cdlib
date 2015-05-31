c494f169cc3048f0e3fc523d900cd9f1317806fd
mud <me@my.isp.com>
2015-05-31 13:42:48 -0400

    Include the script which creates empty directories (works around a git limitation)

---
+++ preparedirs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# This creates the directories which the MUD expects to be there,
+# but which are empty (and thus not tracked by git)
+#
+# RUN THIS FROM YOUR lib DIRECTORY
+#
+mkdir -p d/Standard/ateam
+mkdir -p d/Standard/log
+mkdir -p d/Standard/open
+mkdir -p d/Standard/domain/private
+mkdir -p syslog/backup
+mkdir -p syslog/log/enter
+mkdir -p players/messages
+mkdir -p players/sanctions/Standard
+mkdir -p players/mail
+mkdir -p d.new/Standard/ateam
+mkdir -p d.new/Standard/log
+mkdir -p d.new/Standard/open
+mkdir -p d.new/Standard/domain/private
+mkdir -p w
+mkdir -p log
